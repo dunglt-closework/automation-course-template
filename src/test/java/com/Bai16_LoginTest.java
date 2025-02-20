5dc42024b26900822aaa157df9662ab0c6fd944e
dunglt-closework <dunglt@closerworks.com>
2025-02-20 20:54:42 +0700

    B16_LoginTest

---
+++ src/test/java/com/Bai16_LoginTest.java
@@ -0,0 +1,51 @@
+package com;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.utils.BasicTest;
+
+public class Bai16_LoginTest extends BasicTest {
+
+    @Test()
+    public void loginTestSuccess() throws Exception {
+
+        String url = "https://bantheme.xyz/hathanhauto/tai-khoan/";
+        driver.get(url);
+        Assert.assertEquals(driver.getCurrentUrl(), url);
+
+        WebElement emailField = driver.findElement(By.xpath("//input[@id='username']"));
+        emailField.sendKeys("dunglt.bkhn@gmail.com");
+
+        WebElement passField = driver.findElement(By.xpath("//input[@id='password']"));
+        passField.sendKeys("dung1992@");
+
+        WebElement loginButton = driver.findElement(By.xpath("//button[@name='login']"));
+        loginButton.click();
+
+        WebElement loginSuccess = driver.findElement(By.xpath("//li[contains(@class,'logout')]//a"));
+        Assert.assertEquals(loginSuccess.getText(), "Thoát");
+
+    }
+
+    @Test()
+    public void loginTestFail() throws Exception {
+
+        String url = "https://bantheme.xyz/hathanhauto/tai-khoan/";
+        driver.get(url);
+        Assert.assertEquals(driver.getCurrentUrl(), url);
+
+        WebElement emailField = driver.findElement(By.xpath("//input[@id='username']"));
+        emailField.sendKeys("dunglt.bkhn@gmail.com");
+
+        WebElement loginButton = driver.findElement(By.xpath("//button[@name='login']"));
+        loginButton.click();
+
+        WebElement ErrorMessage = driver.findElement(By.xpath("//ul[@class='woocommerce-error']"));
+        Assert.assertEquals(ErrorMessage.getText(), "Lỗi: Mục nhập mật khẩu trống.");
+
+    }
+    
+}
